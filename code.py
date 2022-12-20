3b920c0e58990b4f8f55d2eafd0fef92cdfd3537
Lia Duggan <lia.duggan@stu.ocsb.ca>
2022-12-20 15:18:54 +0000

    The Sprite moves with buttons

--- code.py
+++ code.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+#!/usr/bin/env python3
 # Created by: Lia Duggan
 # Created on: Dec 15th, 2022
 # A version of Space Aliens on PyBadge
@@ -13,6 +14,7 @@ def game_scene():
     # imported an image and put it into a variable
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
+
     # grid of an image background
     background = stage.Grid(image_bank_background, 10, 8)
 
@@ -25,6 +27,23 @@ def game_scene():
 
     while True:
         # get user input i.e buttons click
+        keys = ugame.buttons.get_pressed()
+        if keys and ugame.K_K:
+            print("A")
+        if keys and ugame.K_O:
+            print("B")
+        if keys and ugame.K_START:
+            print("Start")
+        if keys and ugame.K_SELECT:
+            print("Select")
+        if keys and ugame.K_RIGHT:
+            bird.move(bird.x + 1, bird.y)
+        if keys and ugame.K_LEFT:
+            bird.move(bird.x - 1, bird.y)
+        if keys and ugame.K_UP:
+            bird.move(bird.x, bird.y - 1)
+        if keys and ugame.K_DOWN:
+            bird.move(bird.x, bird.y + 1)
 
         # Update game Logic
 
