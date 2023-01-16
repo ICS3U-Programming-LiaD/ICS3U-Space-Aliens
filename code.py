069578d1a40731c8b0e58fe983ead16dd3b0c44a
Lia Duggan <lia.duggan@stu.ocsb.ca>
2023-01-16 19:12:05 +0000

    Splash screen

--- code.py
+++ code.py
@@ -8,12 +8,90 @@
 import constants
 import stage
 import ugame
+import time
+import random
+
+
+def splash_scene():
+
+    start_sound = open("coin.wav", "rb")
+    sound = ugame.audio
+    sound.stop()
+    sound.mute(False)
+    sound.play(start_sound)
+
+    # imported an image and put it into a variable
+    image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
+
+    # sets the background image to
+    background = stage.Grid(
+        image_bank_mt_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y
+    )
+    background.tile(2, 2, 0)  # blank white
+
+    background.tile(3, 2, 1)
+
+    background.tile(4, 2, 2)
+
+    background.tile(5, 2, 3)
+
+    background.tile(6, 2, 4)
+
+    background.tile(7, 2, 0)  # blank white
+
+    background.tile(2, 3, 0)  # blank white
+
+    background.tile(3, 3, 5)
+
+    background.tile(4, 3, 6)
+
+    background.tile(5, 3, 7)
+
+    background.tile(6, 3, 8)
+
+    background.tile(7, 3, 0)  # blank white
+
+    background.tile(2, 4, 0)  # blank white
+
+    background.tile(3, 4, 9)
+
+    background.tile(4, 4, 10)
+
+    background.tile(5, 4, 11)
+
+    background.tile(6, 4, 12)
+
+    background.tile(7, 4, 0)  # blank white
+
+    background.tile(2, 5, 0)  # blank white
+
+    background.tile(3, 5, 0)
+
+    background.tile(4, 5, 13)
+
+    background.tile(5, 5, 14)
+
+    background.tile(6, 5, 0)
+
+    background.tile(7, 5, 0)  # blank white
+
+    # The display that will show up and refreshing it with 60 hertz
+    game = stage.Stage(ugame.display, constants.FPS)
+    game.layers = [background]
+    game.render_block()
+
+    # game loop to repeat forever
+    while True:
+        # Wait 2 seconds
+        time.sleep(2.0)
+        # Go to the menu scene
+        menu_scene()
 
 
 def menu_scene():
 
     # imported an image and put it into a variable
-    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
 
     # The Text Objects
     text = []
@@ -21,22 +99,20 @@ def menu_scene():
         width=29, height=12, font=None, palette=constants.Red_PALETTE, buffer=None
     )
     text1.move(20, 10)
-    text1.text("Lia's game")
+    text1.text("Flying Bird")
     text.append(text1)
 
     text2 = stage.Text(
         width=29, height=12, font=None, palette=constants.Red_PALETTE, buffer=None
     )
     text2.move(40, 110)
-    text2.text("Press Start to Play")
+    text2.text("Press Start")
     text.append(text2)
 
     # grid of an image background
     background = stage.Grid(
-        image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y
+        image_bank_mt_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y
     )
-
-    background = stage.Grid(image_bank_background, 10, 8)
 
     # The display that will show up and refreshing it with 60 hertz
     game = stage.Stage(ugame.display, constants.FPS)
@@ -67,7 +143,6 @@ def game_scene():
 
     # Adding in the Sprites
     bird = stage.Sprite(image_bank_sprites, 4, 75, 66)
-
     cloud = stage.Sprite(
         image_bank_sprites,
         9,
@@ -87,8 +162,6 @@ def game_scene():
     sound.stop()
     sound.mute(False)
 
-    background = stage.Grid(image_bank_background, 10, 8)
-
     # The display that will show up and refreshing it with 60 hertz
     game = stage.Stage(ugame.display, constants.FPS)
     game.layers = [bird] + [cloud] + [background]
@@ -99,8 +172,8 @@ def game_scene():
         # get user input i.e buttons click
         keys = ugame.buttons.get_pressed()
 
-        # Doing nothing with the B button
-        if keys & ugame.K_X != 0:
+        # A button to fire
+        if keys & ugame.K_O != 0:
             # Changing the state of the A button
             if a_button == constants.button_state["button_up"]:
                 a_button = constants.button_state["button_just_pressed"]
@@ -111,13 +184,17 @@ def game_scene():
                 a_button = constants.button_state["button_released"]
             else:
                 a_button = constants.button_state["button_up"]
-                # A button to fire
-        if keys & ugame.K_O != 0:
-            pass
-        if keys & ugame.K_START != 0:
-            print("Start")
-        if keys & ugame.K_SELECT != 0:
-            print("Select")
+
+                # Doing nothing with the B button
+            if keys & ugame.K_X:
+                pass
+            if keys & ugame.K_O:
+                pass
+            if keys & ugame.K_START:
+                pass
+            if keys & ugame.K_SELECT:
+                pass
+
         if keys & ugame.K_RIGHT:
             # moves the sprite to the right
             if bird.x <= constants.SCREEN_X - 16:
@@ -153,7 +230,6 @@ def game_scene():
             # Plays the shooting sound when A is pressed
         if a_button == constants.button_state["button_just_pressed"]:
             sound.play(bird_sound)
-
         # redraw Bird
         game.render_sprites([bird] + [cloud])
         game.tick()
@@ -162,4 +238,4 @@ def game_scene():
 
 
 if __name__ == "__main__":
-    game_scene()
+    splash_scene()
--- constants.py
+++ constants.py
@@ -21,7 +21,7 @@ button_state = {
     "button_up": "up",
     "button_just_pressed": "just pressed",
     "button_still_pressed": "still pressed",
-    "button_released": "released",
+    "button_released": "released"
 }
 
 # New palette for filled Text
